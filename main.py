9a1591d6f25ef35e9e65f150800ee34dc602793d
maw <maw@maw.de>
2023-06-30 11:48:16 +0200

    transcribe in background, show progress

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import whisper
 from docx import Document
 import os, subprocess, platform
 from os.path import exists
+import threading
+import time
 
 def find_new_filepath():
     fpath = 'doc-{}.docx'
@@ -23,7 +25,17 @@ def launch_path(fpath):
     else:
         subprocess.call(('xdg-open', fpath))
 
-def transcribe(file_path):
+def update_progress_bar(win, stop):
+    while True:
+        if stop():
+            break
+        win.write_event_value('update_bar', 1)
+        time.sleep(.5)
+
+def transcribe(file_path, window):
+    stop_thread = False
+    thread2 = threading.Thread(target=update_progress_bar, args=(window, lambda: stop_thread))
+    thread2.start()
     model = whisper.load_model('medium')
     result = model.transcribe(file_path, language='de')
     text = result['text']
@@ -32,16 +44,51 @@ def transcribe(file_path):
     fpath = find_new_filepath()
     document.save(fpath)
     launch_path(fpath)
+    stop_thread = True
+    thread2.join(timeout=0)
+    window.write_event_value('-THREAD-', 'Finished')
 
-layout = [[sg.Text('Audiodatei auswählen'), sg.FileBrowse(key='-IN-')], [sg.Button("OK")]]
-window = sg.Window('Audio Transkribieren', layout, size=(300,200))   
+layout = [[sg.Text('Audiodatei auswählen', size=(30,1)), sg.FileBrowse(key='-IN-')], 
+          [sg.Text('', size=(30,1)), sg.Button('Transkribieren')],
+          [sg.Text('Fortschritt', key='bar_label'), sg.ProgressBar(100, size=(20, 20), orientation='h', key='bar')],
+          ]
+
+window = sg.Window('Audio Transkribieren', layout, size=(400,100), finalize=True)   
+
+thread = None
+progress = 0
+step = 2
+
+def show_progress_bar(bool):
+    window['bar_label'].update(visible=bool)
+    window['bar'].update(visible=bool)
+
+show_progress_bar(False)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
+        if thread:
+            thread.join(timeout=0)
         break
-    elif event == 'OK':
+    elif event == 'Transkribieren' and not thread:
         path = values['-IN-']
-        transcribe(path)
+        if path:
+            show_progress_bar(True)
+            window['Transkribieren'].update(disabled=True)
+            thread = threading.Thread(target=transcribe, args=(path, window))
+            thread.start()
+    elif event == '-THREAD-':
+        thread.join(timeout=0)
+        thread = None
+        progress = 0
+        window['bar'].update_bar(0,0)
+        show_progress_bar(False)
+        window['Transkribieren'].update(disabled=False)
+    elif event == 'update_bar':
+        window['bar'].update_bar(progress)
+        progress = progress + step
+        if progress > 100 or progress < 0:
+            step *= -1
 
 window.close()
