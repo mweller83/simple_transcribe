7a5ce98a7615a45951738d0fd5c26d589232192a
maw <maw@maw.de>
2023-06-30 10:23:21 +0200

    do not overwrite older docx files

--- main.py
+++ main.py
@@ -2,22 +2,36 @@ import PySimpleGUI as sg
 import whisper
 from docx import Document
 import os, subprocess, platform
+from os.path import exists
 
-def transcribe(file_path):
-    model = whisper.load_model('medium')
-    result = model.transcribe(file_path, language='de')
-    text = result['text']
-    #print(text)
-    document = Document()
-    document.add_paragraph(text)
-    fpath = 'doc.docx'
-    document.save(fpath)
+def find_new_filepath():
+    fpath = 'doc-{}.docx'
+    new_fpath = ''
+    count = 0
+    while True:
+        new_fpath = fpath.format(count)
+        if not exists(new_fpath):
+            break
+        count = count + 1
+    return new_fpath
+
+def launch_path(fpath):
     if platform.system() == 'Darwin':
         subprocess.call(('open', fpath))
     elif platform.system() == 'Windows':
         os.startfile(fpath)
     else:
         subprocess.call(('xdg-open', fpath))
+
+def transcribe(file_path):
+    model = whisper.load_model('medium')
+    result = model.transcribe(file_path, language='de')
+    text = result['text']
+    document = Document()
+    document.add_paragraph(text)
+    fpath = find_new_filepath()
+    document.save(fpath)
+    launch_path(fpath)
 
 layout = [[sg.Text('Audiodatei auswählen'), sg.FileBrowse(key='-IN-')], [sg.Button("OK")]]
 window = sg.Window('Audio Transkribieren', layout, size=(300,200))   
@@ -28,7 +42,6 @@ while True:
         break
     elif event == 'OK':
         path = values['-IN-']
-        #print(path)
         transcribe(path)
 
 window.close()
